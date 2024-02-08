8ccb7e5995bb5e7f6c55123bd7c544a3c0d6475f
tamila_popova <popova.t.t@yandex.ru>
2024-02-08 12:10:07 +0300

    Task TodoList complete

--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -1,9 +1,66 @@
 package practice;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     private static TodoList todoList = new TodoList();
 
     public static void main(String[] args) {
-        // TODO: написать консольное приложение для работы со списком дел todoList
+        while (true) {
+            String regex = "\\d+";
+            String sc = new Scanner(System.in).nextLine();
+            String[] text = sc.split(" ", 3);
+            switch (text[0]) {
+                case "ADD" -> {
+                    if (text.length <= 1) {
+                        System.out.println("-");
+                    } else {
+                        if (text[1].matches(regex)) {
+                            todoList.add(Integer.parseInt(text[1]), text[2]);
+                            System.out.println("Добавлено дело " + '"' + text[2] + '"');
+                        } else {
+                            String sum = text[1].concat(" ").concat(text[2]);
+                            System.out.println("Добавлено дело " + '"' + sum + '"');
+                            todoList.add(sum);
+                        }
+                    }
+                }
+                case "LIST" -> {
+                    todoList.getTodos();
+                    ArrayList<String> todos = todoList.getTodos();
+                    if (text.length <= 1) {
+                        System.out.println("-");
+                    }
+                    for (int i = 0; i < todos.size(); i++) {
+                        System.out.println(todos.get(i));
+                    }
+                }
+                case "DELETE" -> {
+                    int num = Integer.parseInt(text[1]);
+                    int index = todoList.getTodos().size();
+                    if (text.length <= 1) {
+                        System.out.println("-");
+                    }
+                    if (text[1].matches(regex) && num >= 0 && num <= index) {
+                        todoList.delete(num);
+                        System.out.println("Дело " + '"' + text[2] + '"' + "удалено");
+                    } else {
+                        System.out.println("Дело с таким номером не существует");
+                    }
+                }
+                case "EDIT" -> {
+                    int num = Integer.parseInt(text[1]);
+                    int index = todoList.getTodos().size();
+                    if (text.length <= 1) {
+                        System.out.println("-");
+                    }
+                    if (text[1].matches(regex) && num >= 0 && num < index) {
+                        todoList.edit(Integer.parseInt(text[1]), text[2]);
+                        System.out.println("Дело " + '"' + Integer.parseInt(text[1]) + '"' + "заменено на " + '"' + text[2] + '"');
+                    }
+                }
+            }
+        }
     }
 }
--- src/main/java/practice/TodoList.java
+++ src/main/java/practice/TodoList.java
@@ -1,31 +1,38 @@
 package practice;
-
 import java.util.ArrayList;
 
 public class TodoList {
+    private final ArrayList<String> todos;
+
+    public TodoList() {
+        todos = new ArrayList<>();
+    }
 
     public void add(String todo) {
-        // TODO: добавьте переданное дело в конец списка
+        todos.add(todo);
     }
 
     public void add(int index, String todo) {
-        // TODO: добавьте дело на указаный индекс,
-        //  проверьте возможность добавления
+        if (index >= 0 && index <= todos.size()) {
+            todos.add(index, todo);
+        } else {
+            todos.add(todo);
+        }
     }
 
     public void edit(int index, String todo) {
-        // TODO: заменить дело на index переданным todo индекс,
-        //  проверьте возможность изменения
+        if (index >= 0 && index <= todos.size()) {
+            todos.set(index, todo);
+        }
     }
 
     public void delete(int index) {
-        // TODO: удалить дело находящееся по переданному индексу,
-        //  проверьте возможность удаления дела
+        if (index >= 0 && index < todos.size()) {
+            todos.remove(index);
+        }
     }
 
     public ArrayList<String> getTodos() {
-        // TODO: вернуть список дел
-        return new ArrayList<>();
+        return new ArrayList<>(todos);
     }
-
 }
